a15ab235bd952de61aec3daebf4b13c3cf4e32cb
Rob <rsdates@ncsu.edu>
2018-03-29 03:37:28 -0400

    added record voicemail tab code

---
+++ Code/BMH/ViewControllers/RecordViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RecordViewController.swift
+//  BMH
+//
+//  Created by Robert Dates on 3/29/18.
+//  Copyright © 2018 Bamboo Mobile Health. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RecordViewController: UITableViewController {
+    override func viewDidLoad() {
+        tableView.delegate = self
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RecordCell", for: indexPath)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 177.0
+    }
+    
+}
